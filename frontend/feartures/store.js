2853352a97a59e4e3f00f658d1319ac3f6e955ef
riturajsinghgour14 <148074753+riturajsinghgour14@users.noreply.github.com>
2024-12-11 14:55:26 -0800

    resolve path

--- frontend/feartures/store.js
+++ frontend/feartures/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from"./auth/authSlice";
 import adminReducer from "./admin/adminslice";
-import ticketSlice from "./Ticket/TicketSlice";
+import ticketSlice from "./ticket/ticketSlice";
 const store = configureStore ({
     reducer  : {
         auth : authReducer,
