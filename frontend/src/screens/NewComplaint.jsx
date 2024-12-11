b9cc863011b3d397184a309b98e62f11e4ca2f55
riturajsinghgour14 <148074753+riturajsinghgour14@users.noreply.github.com>
2024-12-11 15:07:11 -0800

    NewComplaint.jsx

--- frontend/src/screens/NewComplaint.jsx
+++ frontend/src/screens/NewComplaint.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
-import { raiseTicket } from '../../feartures/Ticket/TicketSlice';
+// import { raiseTicket } from '../../feartures/Ticket/TicketSlice';
+import { raiseTicket } from '../../feartures/ticket/ticketSlice';
 import BackButton from '../components/BackButton';
 import { toast } from 'react-toastify';
 
