82e1bad60c81d96a77381b529b14465d7be10c3e
riturajsinghgour14 <148074753+riturajsinghgour14@users.noreply.github.com>
2024-12-11 16:54:35 -0800

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy : {
       '/api': {
-        target : 'http://localhost:8081',
+        target : "https://complaintbox-iblq.onrender.com",
         changeOrigin : true,
         secure : false,
       },
